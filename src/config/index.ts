c5785a5b701aa4196cf3f65654fe076fca2056ca
Ibrahim Wudiri <ibwudiri@gmail.com>
2026-03-10 01:14:28 -0700

    add comment to index.ts for clarity

--- src/config/index.ts
+++ src/config/index.ts
@@ -10,3 +10,4 @@ export const ACCESS_TOKEN_EXPIRY = "15m";
 export const REFRESH_TOKEN_EXPIRY = "7d";
 export const FRONTEND_URL = process.env.FRONTEND_URL || "http://localhost:5173";
 export const DIRECT_URL = process.env.DIRECT_URL || "";
+// bcbdaun c 
